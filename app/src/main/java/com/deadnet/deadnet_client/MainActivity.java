d5780b9e7224bce7573fa9068355700fa53522a3
coulterminer <coultercash@gmail.com>
2024-02-03 18:31:00 -0700

    Add Cop Images/Cop Alert

--- app/src/main/java/com/deadnet/deadnet_client/MainActivity.java
+++ app/src/main/java/com/deadnet/deadnet_client/MainActivity.java
@@ -1,10 +1,21 @@
 package com.deadnet.deadnet_client;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private CardView copView;
+
+    void CopAlert(){
+        copView = findViewById(R.id.CopView);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(copView, "translationY", 0f);
+        animator.setDuration(500); // Adjust the duration as needed
+        animator.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
