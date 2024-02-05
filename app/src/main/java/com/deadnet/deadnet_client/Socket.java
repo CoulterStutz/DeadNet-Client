bc86233e3823670524853e780daf694033ece686
coulterminer <coultercash@gmail.com>
2024-02-04 18:30:41 -0700

    [NETWORKING]: Create Constructor for Socket Class

--- app/src/main/java/com/deadnet/deadnet_client/Socket.java
+++ app/src/main/java/com/deadnet/deadnet_client/Socket.java
@@ -1,4 +1,13 @@
 package com.deadnet.deadnet_client;
 
 public class Socket {
+    private String VIN;
+    private int RacerPin;
+    public String RacerName;
+
+    public Socket(String VIN, int RacerPin, String RacerName) {
+        this.VIN = VIN;
+        this.RacerPin = RacerPin;
+        this.RacerName = RacerName;
+    }
 }
